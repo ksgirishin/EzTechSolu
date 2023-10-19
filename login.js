f4080bb327d8c7a000cf32ba70ffa7ad06f234a2
ksgirishin <ksgirishin@gmail.com>
2023-10-19 15:27:26 +0000

    login functionality

---
+++ login.js
@@ -0,0 +1,27 @@
+function login() {
+
+            // This is a basic example and does not perform actual authentication.
+
+            // Replace this logic with your server-side authentication.
+
+            
+
+            var username = document.getElementById("username").value;
+
+            var password = document.getElementById("password").value;
+
+            
+
+            if (username === "your_username" && password === "your_password") {
+
+                alert("Login successful!");
+
+                // Redirect to another page or perform further actions.
+
+            } else {
+
+                alert("Login failed. Please check your username and password.");
+
+            }
+
+        }
